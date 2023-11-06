224afb4621fa4fc70fd36ea6d66c4a44f66b5628
lilaj feiguin <lilaj.feiguin@gmail.com>
2023-11-06 18:31:55 -0300

    agregué todas las validaciones a funciones para pensar en más escabilidad en el futuro y las ejectuto todas al final. También agregué variables de scope global para poder hacer return de valores obtenidos por prompts por fuera de las funciones

--- js/app.js
+++ js/app.js
@@ -1,136 +1,135 @@
-console.log("Conectado");
-
-
-//const MostrarAlert = (mensaje) => {return mensaje;}; AYUDA, NO ME FUNCIONA CON ARROW FUNCTION!
 
 
 const simuladorBtn = document.getElementById("reserva");
 
 simuladorBtn.addEventListener("click", function() {
 
-    const validateEmail = function (email) {
-        let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
-    }
-    
-    const mostrarAlert = function (mensaje){
-        alert (mensaje);
-    }
+    let iHuespedes;
+    let iDias;
 
-    let checked = 0;
+    function validateCantDias () {
+        let checked = 0;
 
-    while (checked === 0) {
-        let cantDias = prompt ("Ingrese la cantidad de días que deseas hospedarte");
-
-        if (cantDias === ""){
-            mostrarAlert ("No ingresaste la cantidad de días que deseas hospedarte");
-        }
-        else if ( parseInt (cantDias) != cantDias) {
-            mostrarAlert ("Ingresa un número válido");
-        }
-        else if ( cantDias < 1) {
-            mostrarAlert ("El número de días no puede ser menor a 1");
-        }
-        else {  
-            mostrarAlert ("Bien, continuemos");
-            console.log (cantDias); 
-            checked = 1;
-        } 
-    }  
-
-    checked = 0;
-
-    let cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
-
-    while (checked === 0) {
-        if (cantPersonas === ""){
-            mostrarAlert ("No ingresaste la cantidad de personas que desean hospedarse");
-            cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
-        }
-        else if ( parseInt (cantPersonas) != cantPersonas) {
-            mostrarAlert ("Ingresa un número válido");
-            cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
-        }
-        else if ( cantPersonas < 1) {
-            mostrarAlert ("La cantidad de personas que desean hospedarse no puede ser menor a 1");
-            cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
-        }
-        else {
-            mostrarAlert ("Bien, continuemos");
-            console.log (cantPersonas); 
-            checked = 1;
-        } 
-    }  
-
-    checked = 0;
-    let nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
-
-    while (checked === 0) {
-        if (nombre1 === ""){
-            mostrarAlert ("No ingresaste nombre y apellido");
-            nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
-        }
-        else if (/^[0-9]+$/.test(nombre1) === true) {
-            mostrarAlert ("Ingresa un nombre y apellido válido sin números");
-            nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
-        }
-        else {
-            mostrarAlert ("Nombre y apellido registrado");
-            console.log (nombre1); 
-            checked = 1;
-        }   
-    }
-
-
-    for (i=1 ; i < cantPersonas; i++) {
-        let nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
-        checked = 0;
         while (checked === 0) {
-            if (nombre2 === ""){
-                mostrarAlert ("No ingresaste el nombre y apellido");
-                nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
+            let cantDias = prompt ("Ingrese la cantidad de días que deseas hospedarte");
+            if (cantDias === ""){
+                alert ("No ingresaste la cantidad de días que deseas hospedarte");
             }
-            else if (/^[0-9]+$/.test(nombre2) === true) {
-                mostrarAlert ("Ingresa un nombre y apellido válido sin números");
-                nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
+            else if ( parseInt (cantDias) != cantDias) {
+                alert ("Ingresa un número válido");
+            }
+            else if ( cantDias < 1) {
+                alert ("El número de días no puede ser menor a 1");
+            }
+            else {  
+                alert ("Bien, continuemos");
+                checked = 1;
+                iDias = cantDias;
+                return iDias; 
+            } 
+        }
+    
+    }
+
+    function validateCantHuesped () {
+        checked = 0;
+
+        let cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
+    
+        while (checked === 0) {
+            if (cantPersonas === ""){
+                alert ("No ingresaste la cantidad de personas que desean hospedarse");
+                cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
+            }
+            else if ( parseInt (cantPersonas) != cantPersonas) {
+                alert ("Ingresa un número válido");
+                cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
+            }
+            else if ( cantPersonas < 1) {
+                alert ("La cantidad de personas que desean hospedarse no puede ser menor a 1");
+                cantPersonas = prompt ("Ingrese la cantidad de personas que desean hospedarse");
             }
             else {
-                mostrarAlert ("Nombre y apellido registrado");
-                console.log (nombre2); 
+                alert ("Bien, continuemos");
+                checked = 1;
+                iHuespedes = cantPersonas;
+                return iHuespedes 
+            } 
+        }  
+    }
+
+    function validateHuesped1 (){
+        checked = 0;
+        let nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
+    
+        while (checked === 0) {
+            if (nombre1 === ""){
+                alert ("No ingresaste nombre y apellido");
+                nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
+            }
+            else if (/^[0-9]+$/.test(nombre1) === true) {
+                alert ("Ingresa un nombre y apellido válido sin números");
+                nombre1 = prompt ("Ingresa el nombre del/ la primer huésped");
+            }
+            else {
+                alert ("Nombre y apellido registrado");
+                checked = 1;
+            }   
+        }
+    }
+
+    function validateHuespedes () {
+        for (i=1 ; i < iHuespedes; i++) {
+            let nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
+            checked = 0;
+            while (checked === 0) {
+                if (nombre2 === ""){
+                    alert ("No ingresaste el nombre y apellido");
+                    nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
+                }
+                else if (/^[0-9]+$/.test(nombre2) === true) {
+                    alert ("Ingresa un nombre y apellido válido sin números");
+                    nombre2 = prompt ("Ingrese el nombre y apellido del/la siguiente huésped");
+                }
+                else {
+                    alert ("Nombre y apellido registrado");
+                    checked = 1;
+                } 
+            }      
+        }
+    }
+
+
+    function validateEmail (){
+        const validateEmail = function (email) {
+            let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return regex.test(email);
+        }
+    
+        checked = 0;
+        let email = prompt ("Por último, ingresa tu email. Te contactaremos con toda la información para completar el pago y asegurar tu reserva");
+    
+        while (checked === 0) {
+            if (email === ""){
+                alert ("No ingresaste tu email");
+                email = prompt ("Ingresa tu email");
+            }
+            else if (!validateEmail (email) ) {
+                alert("La dirección de correo electrónico no es válida.");
+                email = prompt ("Ingresa tu email");        
+            }
+            else {
+                alert ("Quedó registrada tu consulta por una estadía de " + iDias + " días para " + iHuespedes + " personas. A la brevedad, te enviaremos toda la información por email. Gracias!");
                 checked = 1;
             } 
-        }      
+        }   
     }
-            
-
-    checked = 0;
-    let email = prompt ("Por último, ingresa tu email. Te contactaremos con toda la información para completar el pago y asegurar tu reserva");
-
-    while (checked === 0) {
-        if (email === ""){
-            mostrarAlert ("No ingresaste tu email");
-            email = prompt ("Ingresa tu email");
-        }
-        else if (!validateEmail (email) ) {
-            alert("La dirección de correo electrónico no es válida.");
-            email = prompt ("Ingresa tu email");        
-        }
-        else {
-            alert ("A la brevedad, te enviaremos toda la información por email. Gracias!");
-            console.log (email); 
-            checked = 1;
-        } 
-    }   
     
+    
+    validateCantDias ();
+    validateCantHuesped ();
+    validateHuesped1 ();
+    validateHuespedes ();
+    validateEmail ();
+
 });
-
-
-/** sugerencia de Amador para probar luego
-for (i=1;i<=cantPersonas;i++){
-    let nombre = prompt ("Ingresa el nombre del huésped no."+i);
-    while(/^[0-9]+$/.test(nombre)){
-        alert('No ingresaste tu nombre y apellido');
-        nombre = prompt ("Ingresa el nombre del huésped no."+i);
-    }
-    alert('Se registró al huesped No'+i+" :"+nombre)
-} */
